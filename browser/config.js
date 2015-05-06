bc589bf29ae89674e9a9f25093664a68a66cab89
kriserickson <ksoncan34@yahoo.ca>
2015-05-06 13:08:01 -0700

    Initial commmit

---
+++ browser/config.js
@@ -0,0 +1,34 @@
+System.config({
+  "baseURL": "C:\\Users\\kris\\AppData\\Roaming\\npm\\node_modules\\jspm\\lib\\config/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.3.15",
+    "babel": "npm:babel-core@5.2.16",
+    "babel-runtime": "npm:babel-runtime@5.2.16",
+    "bootstrap": "github:twbs/bootstrap@3.3.4",
+    "core-js": "npm:core-js@0.9.6",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "github:twbs/bootstrap@3.3.4": {
+      "jquery": "github:components/jquery@2.1.3"
+    },
+    "npm:core-js@0.9.6": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
---
+++ browser/scripts/splash/app.js
@@ -0,0 +1,5 @@
+import angular from 'angular';  
+import { SourceMapCtrl } from './controller';
+
+angular.module('sourceMapApp', [])
+    .controller('sourceMap', SourceMapCtrl);
